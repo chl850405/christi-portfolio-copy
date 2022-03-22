9d29845b5258cf10eac243d7206422b1581be906
chl850405 <christimarchetti@gmail.com>
2022-03-22 16:38:20 -0400

    add proficiencies to resume page

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -6,7 +6,32 @@ import Pdf from "../assets/resume/Christi Marchetti.pdf";
 function Resume() {
   return (
     <div className="resume" style={{ height: "100vh", width: "84%" }}>
-      <Link to={Pdf} target="_blank" rel="noreferrer noopener" >Click Here to Download My Resume</Link>
+      <p>Resume
+Proficiencies
+<br/>
+Front End,
+Git,
+HTML,
+Handlebars,
+Reactjs,
+CSS,
+Bootstrap,
+Material UI,
+JavaScript,
+JQuery,
+Third Party APIs,
+Server Side APIs,
+Back End,
+OOP,
+MVC,
+ORM,
+Node,
+Express,
+MongoDB,
+PWA
+</p>
+<Link to={Pdf} target="_blank" rel="noreferrer noopener" >
+​Click Here to Download My Resume</Link>
     </div>
   );
 }
